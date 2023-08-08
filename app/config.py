ecd26d4644b29b211321099dc8986f4b27da3158
Minh Tran <minhttran93@gmail.com>
2023-08-08 15:25:09 -0700

    fix migration dev.db being in the wrong directory

--- app/config.py
+++ app/config.py
@@ -11,12 +11,11 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here (for production)
-    # SQLALCHEMY_DATABASE_URI = os.environ.get(
-    #     'DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
     #     'postgres://', 'postgresql://') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(base_dir, "instance", "dev.db")}'
+    #     'sqlite:///' + os.path.join(basedir, 'dev.db')
+    # base_dir = os.path.abspath(os.path.dirname(__file__))
+    # SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(base_dir, "instance", "dev.db")}'
     SQLALCHEMY_ECHO = True
