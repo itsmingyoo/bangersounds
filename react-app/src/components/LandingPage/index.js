ef7101ba84030aaeb60f97fd01f4be40feb7b796
Minh Tran <minhttran93@gmail.com>
2023-08-09 17:06:10 -0700

    add basic css

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./LandingPage.css";
 
 function LandingPage() {
   const allSongs = useSelector((s) => Object.values(s.songs.Songs));
@@ -8,15 +9,22 @@ function LandingPage() {
   return (
     <>
       <h1>Hello, this is the landing page</h1>
-      <div>Recently Played</div>
-      {allSongs &&
-        allSongs.map((s) => (
-          <div>
-            <img src={s.preview_imageURL} />
-            <div>{s.artistId}</div>
-            <div>{s.username}</div>
-          </div>
-        ))}
+      <div id="recently-played__container">
+        <h2>Recently Played</h2>
+        <div id="recently-played__songs">
+          {allSongs &&
+            allSongs.map((s) => (
+              <div>
+                <img
+                  src={s.preview_imageURL}
+                  className="recently-played__images"
+                />
+                <div>{s.artistId}</div>
+                <div>{s.username}</div>
+              </div>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
