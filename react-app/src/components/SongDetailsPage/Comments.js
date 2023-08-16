4f2f2003737a83f135f4622221c7fb9a7fe73719
Minh Tran <minhttran93@gmail.com>
2023-08-16 10:46:59 -0700

    add a function to calculate date difference in current date and when the user commented

--- react-app/src/components/SongDetailsPage/Comments.js
+++ react-app/src/components/SongDetailsPage/Comments.js
@@ -9,20 +9,54 @@ const CommentBox = ({ song, songs, isPlayingState, currentlyPlaying, togglePlayP
   const handleDelete = (c) => {
     dispatch(thunkDeleteComment(song.id, c.id));
   };
+
   return (
     <div id="user-comment__main">
-      {songComments.map((c) => (
-        <>
-          <div id="user-comment__container">
-            <div key={c.id} className="user-displayname">
-              {c.user.displayName}
+      {songComments.map((c) => {
+        // Create Current Date
+        let currentDate = new Date(); //.toString().split(" ");
+        console.log("currentDate", currentDate); // idx 4 is time (hh:mm:ss)
+
+        // Modify CreatedAt to be the same format as the currentDate
+        let commentDate = new Date(c.createdAt); //.toString().split(" "); // idx 4
+        console.log("commentDate", commentDate);
+
+        // Calculate difference and convert from milliseconds to seconds by dividing by 1000
+        const timeDifference = Math.floor((currentDate - commentDate) / 1000);
+        console.log(timeDifference); // seconds
+
+        // Create a "time since comment was posted"
+        let timeAgoString = "";
+        if (timeDifference < 60) {
+          timeAgoString = `${timeDifference} seconds ago`;
+        } else if (timeDifference < 3600) {
+          const minutes = Math.floor(timeDifference / 60);
+          timeAgoString = `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+        } else if (timeDifference < 86400) {
+          const hours = Math.floor(timeDifference / 3600);
+          timeAgoString = `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+        } else if (timeDifference < 2592000) {
+          const days = Math.floor(timeDifference / 86400);
+          timeAgoString = `${days} ${days === 1 ? "day" : "days"} ago`;
+        } else {
+          const months = Math.floor(timeDifference / 2592000);
+          timeAgoString = `${months} ${months === 1 ? "month" : "months"} ago`;
+        }
+        return (
+          <>
+            <div id="user-comment__container">
+              <div>
+                <div key={c.id} className="user-displayname">
+                  {c.user.displayName}
+                </div>
+                <div className="user-comment">{c.comment}</div>
+              </div>
+              <div className="user-comment__date">{timeAgoString}</div>
             </div>
-            <div className="user-comment">{c.comment}</div>
-            <div className="user-comment__date">{c.createdAt}</div>
-          </div>
-          {c.userId === user.id && <button onClick={() => handleDelete(c)}>Delete</button>}
-        </>
-      ))}
+            {c.userId === user.id && <button onClick={() => handleDelete(c)}>Delete</button>}
+          </>
+        );
+      })}
     </div>
   );
 };
