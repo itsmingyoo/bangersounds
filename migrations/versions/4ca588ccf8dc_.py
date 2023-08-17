bb0ae191af7fd7446f73265ebd9bb236e9b7a697
Minh Tran <minhttran93@gmail.com>
2023-08-16 17:18:30 -0700

    deplying

--- migrations/versions/4ca588ccf8dc_.py
+++ migrations/versions/4ca588ccf8dc_.py
@@ -1,13 +1,15 @@
 """empty message
 
 Revision ID: 4ca588ccf8dc
-Revises: 
+Revises:
 Create Date: 2023-08-16 17:08:49.919520
 
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '4ca588ccf8dc'
@@ -58,6 +60,11 @@ def upgrade():
     sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE songs SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
