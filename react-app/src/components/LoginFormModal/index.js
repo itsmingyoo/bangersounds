856fc700ad1ffd0f8103e6116e8eb55b7712fbc1
Minh Tran <minhttran93@gmail.com>
2023-08-18 13:12:05 -0700

    styled sign in modal

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -14,6 +14,7 @@ function LoginFormModal() {
   const [last_name, setLastName] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [showPw, setShowPw] = useState(false);
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
@@ -34,32 +35,21 @@ function LoginFormModal() {
     const errObj = {};
 
     // Edge Case Error handlers
-    if (password !== confirmPassword)
-      errObj.password =
-        "Confirm Password field must be the same as the Password field";
+    if (password !== confirmPassword) errObj.password = "Confirm Password field must be the same as the Password field";
     if (password.length < 6 || confirmPassword.length < 6)
       errObj.password = "Password requires a minimum of 6 characters.";
-    if (username.length < 6)
-      errObj.username = "Username requires a minimum of 6 characters.";
-    if (email.length < 6)
-      errObj.email = "Email requires a minimum of 6 characters.";
-    if (email.length > 32)
-      errObj.email = "Email requires a minimum of 6 characters.";
+    if (username.length < 6) errObj.username = "Username requires a minimum of 6 characters.";
+    if (email.length < 6) errObj.email = "Email requires a minimum of 6 characters.";
+    if (email.length > 32) errObj.email = "Email requires a minimum of 6 characters.";
     if (!email.includes("@")) errObj.email = "Invalid email.";
-    if (username.includes(" "))
-      errObj.username = "Username cannot have a space.";
+    if (username.includes(" ")) errObj.username = "Username cannot have a space.";
     if (password.includes(" ") || confirmPassword.includes(" "))
       errObj.password = "Password can contain unique characters but no space.";
-    if (username.length > 32)
-      errObj.username = "Username must be between 6 and 32 characters";
-    if (first_name.length > 32)
-      errObj.first_name = "First name can be up to 32 characters";
-    if (last_name.length > 32)
-      errObj.last_name = "Last name can be up to 32 characters";
-    if (first_name.includes(" "))
-      errObj.first_name = "First name cannot include spaces";
-    if (last_name.includes(" "))
-      errObj.last_name = "Last name cannot include spaces";
+    if (username.length > 32) errObj.username = "Username must be between 6 and 32 characters";
+    if (first_name.length > 32) errObj.first_name = "First name can be up to 32 characters";
+    if (last_name.length > 32) errObj.last_name = "Last name can be up to 32 characters";
+    if (first_name.includes(" ")) errObj.first_name = "First name cannot include spaces";
+    if (last_name.includes(" ")) errObj.last_name = "Last name cannot include spaces";
 
     // setErrors if there are any
     if (Object.values(errObj).length > 0) {
@@ -68,51 +58,46 @@ function LoginFormModal() {
     }
 
     if (password === confirmPassword) {
-      const data = await dispatch(
-        signUp(username, email, password, first_name, last_name)
-      );
+      const data = await dispatch(signUp(username, email, password, first_name, last_name));
       if (data) {
         setErrors(data);
       } else {
         closeModal();
       }
     } else {
-      setErrors([
-        "Confirm Password field must be the same as the Password field",
-      ]);
+      setErrors(["Confirm Password field must be the same as the Password field"]);
     }
   };
 
+  const toggleShowPassword = () => {
+    setShowPw(!showPw);
+  };
+
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="login-container">
+      <div className="login-div">Log In</div>
+      <form onSubmit={handleSubmit} className="login-modal">
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <label>Email</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <label>Password</label>
+        <input
+          type={showPw ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label className="show-pw-label">
+          <input type="checkbox" onChange={toggleShowPassword} />
+          Show Password
         </label>
         <button type="submit">Log In</button>
       </form>
-    </>
+    </div>
   );
 }
 
