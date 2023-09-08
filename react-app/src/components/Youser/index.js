679279f64fcad16e8095fda88fa9143578df7877
Minh Tran <minhttran93@gmail.com>
2023-09-08 12:11:38 -0700

    not going to use tailwind due to conflicts with current css

--- react-app/src/components/Youser/index.js
+++ react-app/src/components/Youser/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Youser.css";
-import "../../index.css";
 
 function YouserPage() {
-  return <h1 className="">Hello Testing Tailwind</h1>;
+  return <h1 className="text-3xl text-blue-500">Hello Testing Tailwind</h1>;
 }
 
 export default YouserPage;
