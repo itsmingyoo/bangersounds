74efa6aa40c5cdcb814985e0fdb9d6b5bff019a2
Minh Tran <minhttran93@gmail.com>
2023-09-26 20:11:40 -0700

    initialize playlists component

--- react-app/src/components/Profile/PlayLists.js
+++ react-app/src/components/Profile/PlayLists.js
@@ -4,8 +4,17 @@ import SongDisplay from "../SongDisplay";
 
 const Playlists = () => {
   const dispatch = useDispatch();
-  const playlists = useSelector((s) => s.songs.Songs);
-  console.log(playlists);
+  const user = useSelector((s) => s.session.user);
+  const playlists = useSelector((s) => Object.values(s.playlists));
+  console.log("u", user);
+  console.log("p", playlists);
+  const userPlaylists = [];
+  playlists?.forEach((p) => {
+    if (p.createdBy === user.id) {
+      userPlaylists.push(p);
+    }
+  });
+  console.log("up", userPlaylists[0].songs); //{"1": 1, "2": 2, "3": 3}
 
   // return <div><SongDisplay/></div>>
   return <div>Playlists</div>;
