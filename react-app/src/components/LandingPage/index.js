ebf79864dc4847a1c6fed0bee692992fea18c73b
Minh Tran <minhttran93@gmail.com>
2023-12-16 20:08:44 -0800

    remove console logs!!!!

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -11,16 +11,16 @@ function LandingPage() {
   const dispatch = useDispatch();
 
   const songs = useSelector((s) => Object.values(s.songs.Songs));
-  console.log("LANDING - songs: ", songs);
+  // console.log("LANDING - songs: ", songs);
 
   const isPlayingState = useSelector((s) => s.songs.isPlaying);
-  console.log("LANDING - isPlayingState: ", isPlayingState);
+  // console.log("LANDING - isPlayingState: ", isPlayingState);
 
   const currentlyPlaying = useSelector((s) => s.songs.CurrentlyPlaying);
-  console.log("LANDING - currentlyPlaying: ", currentlyPlaying);
+  // console.log("LANDING - currentlyPlaying: ", currentlyPlaying);
 
   const previousSong = useSelector((s) => s.songs.PreviousSong);
-  console.log("LANDING - previousSong: ", previousSong);
+  // console.log("LANDING - previousSong: ", previousSong);
 
   const togglePlayPause = async (song) => {
     dispatch(playUserSongAction(song));
