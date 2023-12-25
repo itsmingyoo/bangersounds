eb19ceb6f22d246793e1fa1bdea6215ed526a68a
Minh Tran <minhttran93@gmail.com>
2023-12-25 10:13:12 -0800

    delete song modal

--- react-app/src/components/DeleteSongModal/index.js
+++ react-app/src/components/DeleteSongModal/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./DeleteSongModal.css";
 import { useHistory } from "react-router-dom";
 import { thunkDeleteUserSong } from "../../store/songs";
+import ProfilePicture from "../SongDetailsPage/ProfilePicture";
 
 // forgot why i passed in user props... feature coming soon
 function DeleteSongModal({ songId, user }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const { closeModal } = useModal();
+  // const user = useSelector((s) => s.session.user);
+  // const userRef = useRef(user);
+  // useEffect(() => {
+  //   userRef.current = user;
+  // });
 
   const handleClick = async (e) => {
     e.preventDefault();
@@ -24,47 +30,16 @@ function DeleteSongModal({ songId, user }) {
 
   return (
     <div id="delete-modal__container">
-      {/* TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP TOP */}
-      <div id="delete-modal__top">
-        <div id="delete-modal__top-left">PROFILE PICTURE</div>
-        <div id="delete-modal__top-right">
-          INSERT COMPONENT WITH PLAY BUTTON, SONG NAME, ARTIST NAME, DATE, AND WAVEFORMS
-        </div>
-      </div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/* BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM BOTTOM */}
       <div id="delete-modal__bot">
-        {/*  */}
-        {/*  */}
-        {/*  */}
-        {/* LEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT */}
         <div id="delete-modal__bot-left">
-          {/* SLIGHTLY LARGER FONT THAN THE P TAGS */}
           <div>Permanently delete this track?</div>
           <p>
-            Removing this track is irreversible. You will lose all the plays, likes and comments for this track with no
-            way to get them back.
+            Removing this track is irreversible. You will lose all the plays,
+            likes and comments for this track with no way to get them back.
           </p>
-          {/* BUTTONS NO BG - COLOR BLK - NO BORDER */}
           <div id="delete-modal__bot-left-buttons">
             <button onClick={() => closeModal()}>Cancel</button>
             <button onClick={handleClick}>Delete forever</button>
-          </div>
-        </div>
-        {/*  */}
-        {/*  */}
-        {/*  */}
-        {/* RIGHT RIGHT RIGHT RIGHT RIGHT RIGHT RIGHT RIGHT RIGHT */}
-        <div id="delete-modal__bot-right">
-          <p>
-            Never have to delete a track again. Unlock limitless upload time and the ability to replace tracks with a
-            Pro plan.
-          </p>
-          {/* ORANGE BUTTON WHITE TEXT */}
-          <div id="delete-modal_bot-right-button">
-            <button onClick={learnMore}>Learn More</button>
           </div>
         </div>
       </div>
